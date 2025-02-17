d38f3c639db4d8f104767834afbb9797908ed7cf
florian <florianbarjon@icloud.com>
2025-02-17 15:07:05 +0100

    tape

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -3,40 +3,61 @@ import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
 import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
 import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import { SectionHeader } from "@/components/SectionHeader";
+import Image from "next/image";
+import grainImage from "@/assets/images/grain.jpg";
 
 const testimonials = [
   {
     name: "Alex Turner",
-    position: "Marketing Manager @ TechStartups",
-    text: "Alex was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We're thrilled with the results!",
+    position: "Responsable Marketing @ TechStartups",
+    text: "Florian a joué un rôle clé dans la transformation de notre site web en un puissant outil marketing. Son attention aux détails et sa capacité à comprendre notre marque sont exceptionnelles. Nous sommes ravis des résultats !",
     avatar: memojiAvatar1,
   },
   {
-    name: "Olivia Green",
-    position: "Head of Design @ GreenLeaf",
-    text: "Working with Alex was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
+    name: "Cyrielle Schepens",
+    position: "CEO @ En Scènes Acting",
+    text: "Travailler avec Florian a été un véritable plaisir. Son expertise en développement frontend a donné vie à nos designs d'une manière que nous n'aurions jamais imaginée. Le site a dépassé nos attentes.",
     avatar: memojiAvatar2,
   },
   {
-    name: "Daniel White",
-    position: "CEO @ InnovateCo",
-    text: "Alex's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
+    name: "John Doe",
+    position: "PDG @ InnovateCo",
+    text: "La capacité de Florian à créer des expériences utilisateur fluides est inégalée. Notre site a connu une augmentation significative des conversions depuis le lancement du nouveau design. Nous ne pourrions pas être plus heureux.",
     avatar: memojiAvatar3,
   },
   {
-    name: "Emily Carter",
-    position: "Product Manager @ GlobalTech",
-    text: "Alex is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
+    name: "Kristel Feron",
+    position: "PDG @ KrisLaVoixDesAnges",
+    text: "Florian est un véritable magicien du frontend. Il a transformé notre produit complexe en une interface utilisateur intuitive et engageante. Nous recevons déjà des retours positifs de nos clients.",
     avatar: memojiAvatar4,
   },
   {
-    name: "Michael Brown",
-    position: "Director of IT @ MegaCorp",
-    text: "Alex's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
+    name: "Brandon Johnson",
+    position: "Directeur IT @ MegaCorp",
+    text: "Le travail de Florian sur notre site web a été exceptionnel. C'est un développeur talentueux qui est aussi un excellent communicateur. Nous le recommandons vivement.",
     avatar: memojiAvatar5,
   },
 ];
 
 export const TestimonialsSection = () => {
-  return <div>Testimonials Section</div>;
+  return (
+    <div className="py-16">
+  <div className="container">
+    <SectionHeader eyebrow='Clients satisfaits' title="Ce que mes clients disent de moi" description="Ne vous fiez pas seulement à mes dires. Découvrez ce que mes clients pensent de mon travail." />
+  
+    <div>
+      {testimonials.map((testimonial) => 
+        (<div key={testimonial.name} className="bg-gray-800 rounded-3xl p-6">
+          <div className="absolute inset-0" style={{backgroundImage: `url(${grainImage.src})`}}/>
+          <Image src={testimonial.avatar} alt={testimonial.name} />
+          <div>{testimonial.name}</div>
+          <div>{testimonial.position}</div>
+          <p>{testimonial.text}</p>
+        </div>
+        ))}
+      </div>
+  </div>
+  </div>
+  );
 };
