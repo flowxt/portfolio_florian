b2fc6f9e0d3f4f0e0db0315f33f9209b5d4a2e2c
florian <florianbarjon@icloud.com>
2025-02-17 11:44:24 +0100

    Initial commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return <div className="flex justify-center items-center fixed top-3 w-full z-10">
+    <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+      <a href="#" className="nav-item ">Accueil</a>
+      <a href="#" className="nav-item">Projets</a>
+      <a href="#" className="nav-item">A propos</a>
+      <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</a>
+    </nav>
+  </div>;
+};
