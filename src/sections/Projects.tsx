f480db62adfaf92e6d05854a76726817f8f3fbd3
florian <florianbarjon@icloud.com>
2025-02-17 12:41:51 +0100

    image

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,81 +1,94 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
+import ecranFront from "@/assets/images/ecran-front2.png";
+import kris from "@/assets/images/kris.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import Image from "next/image";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
+    company: "En Scènes Acting",
+    year: "2025",
+    title: "En Scènes Acting",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: "Expérience utilisateur améliorée de 60%" },
+      { title: "Vitesse du site augmentée de 40%" },
+      { title: "Trafic mobile accru de 45%" },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
-    image: darkSaasLandingPage,
+    link: "https://cyrielle-app.vercel.app/",
+    image: ecranFront,
   },
   {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
+    company: "Kris LaVoixDesAnges",
+    year: "2025",
+    title: "Kris La Voix Des Anges",
     results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+      { title: "Expension des clients de 20%" },
+      { title: "Augmentation des ventes de 35%" },
+      { title: "Visibilité de la marque accrue de 55%" },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
-    image: lightSaasLandingPage,
+    link: "https://krisnextjs.vercel.app/",
+    image: kris,
   },
-  {
-    company: "Quantum Dynamics",
-    year: "2023",
-    title: "AI Startup Landing Page",
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
-    ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
-    image: aiStartupLandingPage,
-  },
+  // {
+  //   company: "Quantum Dynamics",
+  //   year: "2023",
+  //   title: "AI Startup Landing Page",
+  //   results: [
+  //     { title: "Enhanced user experience by 40%" },
+  //     { title: "Improved site speed by 50%" },
+  //     { title: "Increased mobile traffic by 35%" },
+  //   ],
+  //   link: "https://youtu.be/Z7I5uSRHMHg",
+  //   image: aiStartupLandingPage,
+  // },
 ];
 
 export const ProjectsSection = () => {
-  return <div>
+  return (
+  <section className="pb-16">
+  <div>
     <div className="container">
       <div className="flex justify-center">
       <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center">Un impact réel</p>
       </div>
-      <h2 className="font-serif text-3xl text-center mt-6">Projets à la une</h2>
-      <p className="text-center text-white/60 mt-4">Découvrez ma façon de transformer des idées en expériences digitales immersives.</p>
-      <div className="flex flex-col mt-10">
+      <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Projets à la une</h2>
+      <p className="text-center md:text-lg text-white/60 mt-4 max-w-md mx-auto">Découvrez ma façon de transformer des idées en expériences digitales immersives.</p>
+      <div className="flex flex-col mt-10 md:mt-20 gap-20">
         {portfolioProjects.map((project) => (
-          <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden  after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 p-8">
-            <div className="flex">
+          <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden  after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none">
+        <div className="absolute inset-0 -z-10 opacity-5" style={{
+              backgroundImage: `url(${grainImage.src})`,
+             }}></div>
               <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text ">
               <span>{project.company}</span>
               <span>&bull;</span>
               <span>{project.year}</span>
               </div>
-               </div>
-          <h3 className="font-serif text-2xl mt-2">{project.title}</h3>     
+            
+          <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>     
           <hr className="border-t-2 border-white/5 mt-4" />
-          <ul>
+          <ul className="flex flex-col gap-4 mt-4">
             {project.results.map(result =>(
-              <li>
+              <li className="flex gap-2 text-sm text-white/50">
+                <CheckCircleIcon className='size-5' />
                 <span>{result.title}</span>
                 </li>
             ))}
           </ul>
           <a href={project.link}>
-          <button>Accéder au site</button>
+          <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+            <span>Accéder au site</span>
+<ArrowUpRightIcon className='size-4' />
+            </button>
           </a>
-          <Image src={project.image} alt={project.title} />
+          <Image src={project.image} alt={project.title} className="mt-8 -mb-4" />
           </div>
         ))}
       </div>
     </div>
-  </div>;
+  </div>
+  </section>
+  );
 };
