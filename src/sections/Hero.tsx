26afbb5c276ba93b39556ab32a6017782bf913ae
florian <florianbarjon@icloud.com>
2025-02-19 17:10:21 +0100

    fin projet

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -68,7 +68,7 @@ export const HeroSection = () => {
           <ArrowDown className='size-4'/>
         </button>
         <a 
-  href="mailto:floriandev74@gmail.com?subject=Nouveau%20Projet" 
+  href="#contact" 
   className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl"
 >
   <span>👋</span>
