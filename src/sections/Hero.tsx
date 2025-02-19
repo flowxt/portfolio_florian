7bdfcf920e8bd6b4446547d945c3ead3e701735b
florian <florianbarjon@icloud.com>
2025-02-19 16:56:32 +0100

    bouton contact

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -67,10 +67,13 @@ export const HeroSection = () => {
           <span className="font-semibold">Explores mes projets</span>
           <ArrowDown className='size-4'/>
         </button>
-        <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
-          <span>👋</span>
-          <span className="font-semibold">Contactez-moi</span>
-        </button>
+        <a 
+  href="mailto:floriandev74@gmail.com?subject=Nouveau%20Projet" 
+  className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl"
+>
+  <span>👋</span>
+  <span className="font-semibold">Contactez-moi</span>
+</a>
       </div>
       </div>
   </div>
