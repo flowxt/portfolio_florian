234f0d7f90b506af128241d53926c2c1bac6de7b
florian <florianbarjon@icloud.com>
2025-02-25 13:47:29 +0100

    lien footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -14,23 +14,27 @@ const footerLinks = [
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className='relative -z-10 overflow-x-clip'>
-      <div className='absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10'></div>
-      <div className="container">
-        <div className='border-t border-white/15 py-6 text-sm flex flex-col md:flex-row 
-        md:justify-between items-center gap-8'>
-          <div className='text-white/40'>&copy; {currentYear} Florian Barjon. Tous droits réservés.</div>
-          <nav className='flex flex-col md:flex-row items-center gap-8'>
+    <footer className='relative overflow-x-clip'> {/* Supprimé le -z-10 ici */}
+    <div className='absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10'></div>
+    <div className="container relative z-10"> {/* Ajouté un z-index positif ici */}
+      <div className='border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8'>
+        <div className='text-white/40'>&copy; {currentYear} Florian Barjon. Tous droits réservés.</div>
+        <nav className='flex flex-col md:flex-row items-center gap-8'>
           {footerLinks.map((link) => (
-            <a href={link.href} key={link.title} target="_blank"
-            rel="noopener noreferrer" className='inline-flex items-center gap-1.5'>
+            <a 
+              href={link.href} 
+              key={link.title} 
+              target="_blank"
+              rel="noopener noreferrer" 
+              className='inline-flex items-center gap-1.5 hover:text-emerald-300 transition-colors'
+            >
               <span className='font-semibold'>{link.title}</span>
               <ArrowUpRightIcon className="size-4"/>
             </a>
           ))}
-          </nav>
-        </div>
+        </nav>
       </div>
-    </footer>
+    </div>
+  </footer>
   )
 };
